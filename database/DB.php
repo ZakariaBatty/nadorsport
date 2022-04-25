de5435a6ff2c4507ed2f178f53685e69fc9ca052
ZakariaBAtty <zbatty0912@gmail.com>
2022-04-25 02:22:01 +0000

    add sql and passowrd

--- database/DB.php
+++ database/DB.php
@@ -5,7 +5,7 @@ class DB
     static public function connect()
     {
 
-        $db = new PDO('mysql:host=localhost;dbname=nadorsport', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=nadorsport', 'nadorsport', '7Yjq6z5#');
         $db->exec('set names utf8');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         return $db;
