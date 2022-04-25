64769da183f46ba2998e645cd54ae72aa4d7bbde
ZakariaBAtty <zbatty0912@gmail.com>
2022-04-25 17:50:54 +0000

    change

--- database/DB.php
+++ database/DB.php
@@ -5,8 +5,8 @@ class DB
     static public function connect()
     {
 
-        // $db = new PDO('mysql:host=localhost;dbname=nadorsport', 'nadorsport', '7Yjq6z5#');
-        $db = new PDO('mysql:host=localhost;dbname=nadorsport', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=nadorsport', 'nadorsport', '7Yjq6z5#');
+        // $db = new PDO('mysql:host=localhost;dbname=nadorsport', 'root', '');
         $db->exec('set names utf8');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         return $db;
